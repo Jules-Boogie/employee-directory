abfa16e88605fc0c5cdc4d2eb08b3c0dba413984
Juliet George <georgejuliet11@gmail.com>
2020-05-20 09:25:00 -0700

    Updates

---
+++ precache-manifest.031c7cfa87ef8d44ca31096188add22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4783061cb86043d01d6fc0f4c34b8fae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5be182ca2d1d753109",
+    "url": "/static/js/2.88883df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88883df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8298048e4c4b0993b8b",
+    "url": "/static/js/main.2fb88dc2.chunk.js"
+  },
+  {
+    "revision": "23e1c9b05402499f62c4",
+    "url": "/static/js/runtime-main.62f58a50.js"
+  }
+]);
